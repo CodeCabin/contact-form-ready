ee5906fb4c45aad37b0de3e8607609349b2f50f1
Dylan Auty <dylan@codecabin.co.za>
2020-11-17 10:08:15 +0200

    Fixed privacy button control in settings

--- js/wpcf_tabs.js
+++ js/wpcf_tabs.js
@@ -1,67 +1,26 @@
-jQuery("document").ready(function(){
-    jQuery( "#sola_cfr_tabs" ).tabs({ active: 0 });
-});
+jQuery(function(){
 
-// Matthew Code - Start //
-
-function wpcf_OpenPrivacyTab(){
-    /*
-    var orig_id;
-    for (orig_id = 1; orig_id < 5; orig_id++) {
-        if(orig_id == 4){
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr('tabindex', 0);
-
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").addClass("ui-tabs-active ui-state-active ui-state-focus");
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("aria-selected", true);
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("aria-expanded", true);
-            //jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("onblur","wpcf_ClosePrivacyTab()");
-            jQuery("[id=\"tabs-" +  orig_id + "\"]").attr('aria-hidden', false);
-            jQuery("[id=\"tabs-" +  orig_id + "\"]").css('display', 'block');
-
-        } else{
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr('tabindex', -1);
-
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").removeClass("ui-tabs-active ui-state-active");
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("aria-selected", false);
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("aria-expanded", false);
-            jQuery("[id=\"tabs-" +  orig_id + "\"]").attr('aria-hidden', true);
-            jQuery("[id=\"tabs-" +  orig_id + "\"]").css('display', 'none');
-        }
-    } 
-    */
-
-    jQuery("[aria-labelledby]").on("click", function(){
-        var id = jQuery(this).attr('aria-labelledby');
-        id = id.replace('ui-id-', '');
-        if(parseInt(id) === 4){
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr('tabindex', 0);
-
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").addClass("ui-tabs-active ui-state-active ui-state-focus");
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("aria-selected", true);
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("aria-expanded", true);
-            //jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("onblur","wpcf_ClosePrivacyTab()");
-            jQuery("[id=\"tabs-" +  orig_id + "\"]").attr('aria-hidden', false);
-            jQuery("[id=\"tabs-" +  orig_id + "\"]").css('display', 'block');
-        } else {
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr('tabindex', -1);
-
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").removeClass("ui-tabs-active ui-state-active");
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("aria-selected", false);
-            jQuery("[aria-labelledby=\"ui-id-"+ orig_id + "\"]").attr("aria-expanded", false);
-            jQuery("[id=\"tabs-" +  orig_id + "\"]").attr('aria-hidden', true);
-            jQuery("[id=\"tabs-" +  orig_id + "\"]").css('display', 'none');
-        }
+    jQuery(document).ready(function(){
+        jQuery( "#sola_cfr_tabs" ).tabs({ active: 0 });
     });
-};
 
-function wpcf_ClosePrivacyTab(){
-    jQuery("[aria-labelledby=\"ui-id-4\"]").attr('tabindex', -1);
+    jQuery('#GDPR_privacy_settings_button').on('click', function(){
+        jQuery('.wpcf_gdpr_notice').hide();
+        jQuery('.ui-tabs-nav #ui-id-4').trigger('click');
+    });
 
-    jQuery("[aria-labelledby=\"ui-id-4\"]").removeClass("ui-tabs-active ui-state-active ui-state-focus");
-    jQuery("[aria-labelledby=\"ui-id-4\"]").attr("aria-selected", false);
-    jQuery("[aria-labelledby=\"ui-id-4\"]").attr("aria-expanded", false);
-    jQuery("[id=\"tabs-4\"]").attr('aria-hidden', true);
-    jQuery("[id=\"tabs-4\"]").css('display', 'none');
-}
 
-// Matthew Code - End //
+    jQuery("body").on("click", '.ui-tabs-nav li', function(){
+        var tabId = jQuery(this).attr('aria-controls');
+        tabId = tabId.replace('tabs-', '');
+        tabId = parseInt(tabId);
+        switch(tabId){
+            case 4:
+                jQuery('.wpcf_gdpr_notice').hide();
+                break;
+            default: 
+                jQuery('.wpcf_gdpr_notice').show();
+                break;
+        } 
+    });
+});
